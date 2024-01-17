d3ec482375d7447809b4ecf0ba02e420395ac263
myakisheff <n.s.myakishev@yandex.ru>
2024-01-17 12:11:18 +0300

    Added names of monsters

---
+++ src/EnemyNames.kt
@@ -0,0 +1,7 @@
+class EnemyNames {
+    companion object {
+        private val names : List<String> = listOf("Orc", "Minotaur", "Elf", "Goblin", "Basilisk", "Imp")
+        fun getRandom() : String = names.random()
+    }
+
+}
