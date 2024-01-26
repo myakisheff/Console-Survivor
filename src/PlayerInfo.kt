d09d603b1013e081627d898b230191d85de05609
myakisheff <n.s.myakishev@yandex.ru>
2024-01-26 21:54:21 +0300

    data class for a player info

---
+++ src/PlayerInfo.kt
@@ -0,0 +1,10 @@
+data class PlayerInfo(
+    val level : String,
+    val expPoints : String,
+    val expPointsToUp : String,
+    val skillPoints : String,
+    val healPoints : String,
+    val damage : String,
+    val defense : String,
+    val coordinates : String,
+)
