7a1f9f8c69b383e1f7148adfdd34dcca7fa93290
myakisheff <n.s.myakishev@yandex.ru>
2024-01-27 21:37:57 +0300

    Added new parameters

--- src/PlayerInfo.kt
+++ src/PlayerInfo.kt
@@ -4,7 +4,11 @@ data class PlayerInfo(
     val expPointsToUp : String,
     val skillPoints : String,
     val healPoints : String,
+    val healPointsMax : String,
     val damage : String,
     val defense : String,
+    val regeneration : String,
+    val mana : String,
+    val manaMax : String,
     val coordinates : String,
 )
