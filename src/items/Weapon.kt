fa3f9b28722f4650aaac6094ec80a4c93b6c3ba4
myakisheff <n.s.myakishev@yandex.ru>
2024-02-01 23:23:36 +0300

    Added basic methods

--- src/items/Weapon.kt
+++ src/items/Weapon.kt
@@ -1,5 +1,14 @@
 package items
 
-class Weapon : Equipment() {
-    private var damage : Int = 0
+class Weapon(
+    private val type : WeaponType,
+    private var damage : Int = 0,
+    override var name: String
+) : Equipment() {
+    fun getType() = type
+    fun getDamage() = damage
+
+    fun setDamage(newDamage : Int) {
+        damage = newDamage
+    }
 }
